2b25f5c5f51752f2881fb8db520866495c1898d2
Pedro Flores <pflores@codelab.com.py>
2024-06-01 02:45:49 -0400

    added moar columns info and template generation improved

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import psycopg2
 import inquirer
 import json
 import os
-from caseconverter import camelcase, pascalcase
+from caseconverter import camelcase, pascalcase, snakecase
 
 from cookiecutter.main import cookiecutter
 
@@ -14,7 +14,7 @@ parser = argparse.ArgumentParser(description="Cookiecutter template for a RESTFu
 parser.add_argument('-f', '--overwrite-if-exists', help='Overwrite the contents of the output directory')
 
 TMP_PATH=os.path.dirname(__file__)
-IGNORED_FIELDS = [ "project_slug", "author", "_copy_without_render", "selected_tables", "table", "table_pascal_case", "columns" ]
+IGNORED_FIELDS = [ "project_slug", "author", "_copy_without_render", "selected_tables", "table", "table_pascalcase", "table_snakecase", "columns", "_jinja2_env_vars" ]
 
 def get_postgresql_tables(host, port, db, user, password, schema):
     connection = psycopg2.connect(
@@ -35,6 +35,13 @@ def get_postgresql_tables(host, port, db, user, password, schema):
     connection.close()
     return tables
 
+
+def to_go_data_type(postgres_data_type: str):
+   match postgres_data_type:
+       case 'integer': return 'int'
+       case 'text': return 'string'
+       case _: raise Exception("UNMAPPED DATA_TYPE! {postgres_data_type}")
+
 def get_columns_info(host, port, db, user, password, schema, table):
     connection = psycopg2.connect(
         host=host,
@@ -43,7 +50,20 @@ def get_columns_info(host, port, db, user, password, schema, table):
         user=user,
         password=password
     )
+
     cursor = connection.cursor()
+
+    cursor.execute(f"""
+        SELECT c.column_name
+         FROM information_schema.table_constraints tc
+         JOIN information_schema.constraint_column_usage AS ccu USING (constraint_schema, constraint_name)
+         JOIN information_schema.columns AS c ON c.table_schema = tc.constraint_schema
+           AND tc.table_name = c.table_name AND ccu.column_name = c.column_name
+         WHERE constraint_type = 'PRIMARY KEY' and tc.table_name = '{table}' and tc.table_schema = '{schema}';
+    """)
+
+    pks = [ row[0] for row in cursor.fetchall() ]
+
     cursor.execute(f"""
         SELECT
           a.attname                                       as "column_name",
@@ -60,7 +80,18 @@ def get_columns_info(host, port, db, user, password, schema, table):
               LEFT JOIN pg_catalog.pg_namespace n ON n.oid = c.relnamespace
             WHERE c.relname ~ '^({table})$' AND n.nspname = '{schema}');
     """)
-    columns = [{ "column_name": row[0], "data_type": row[1], "not_null": row[2] } for row in cursor.fetchall()]
+
+    columns = [{
+        "column_name": row[0],
+        "column_name_uppercase": row[0].upper(),
+        "column_name_pascalcase": pascalcase(row[0]),
+        "column_name_snakecase": snakecase(row[0]),
+        "data_type": row[1],
+        "go_data_type": to_go_data_type(row[1]),
+        "not_null": row[2],
+        "primary_key": row[0] in pks
+    } for row in cursor.fetchall()]
+
     cursor.close()
     connection.close()
     return columns
@@ -129,8 +160,9 @@ def main():
         cookiecutter(template=f"{TMP_PATH}/per_table_templates", no_input=True, overwrite_if_exists=True, skip_if_file_exists=True,
                      extra_context={
                          "table": table,
-                         "table_camel_case": camelcase(table),
-                         "table_pascal_case": pascalcase(table),
+                         "table_camelcase": camelcase(table),
+                         "table_pascalcase": pascalcase(table),
+                         "table_snakecase": snakecase(table),
                          "columns": { "values": columns }
                      })
 
--- per_table_templates/{{cookiecutter.project_slug}}/internal/handler/{{cookiecutter.table}}_handler.go
+++ per_table_templates/{{cookiecutter.project_slug}}/internal/handler/{{cookiecutter.table}}_handler.go
@@ -11,87 +11,87 @@ import (
 	"{{cookiecutter.project_slug}}/pkg/db"
 )
 
-// {{cookiecutter.table_pascal_case}} represents the {{cookiecutter.table_pascal_case}} model.
-type {{cookiecutter.table_pascal_case}} struct {
-{{cookiecutter.columns}}
+// {{cookiecutter.table_pascalcase}} represents the {{cookiecutter.table_pascalcase}} model.
+type {{cookiecutter.table_pascalcase}} struct {
 {% for col in cookiecutter.columns["values"] %}
-  {{col['column_name']}}   *{{col['data_type']}}
+  {% if col["primary_key"]: %}
+    {{col['column_name_uppercase']}} *{{col['go_data_type']}}  `json:"{{col['column_name_snakecase']}},omitempty"`
+  {% else %}
+    {{col['column_name_pascalcase']}} {{col['go_data_type']}}  `json:"{{col["column_name_snakecase"]}}"`
+  {% endif %}
 {% endfor %}
-	ID   *int   `json:"id,omitempty"`
-	Name string `json:"name"`
 }
 
-//	 List{{cookiecutter.table_pascal_case}}Handler handles the listing of all heroes.
-//		@Summary		List all heroes
-//		@Description	Lists all heroes in the database
+//	 List{{cookiecutter.table_pascalcase}}Handler handles the listing of all {{cookiecutter.table_snakecase}}s.
+//		@Summary		List all {{cookiecutter.table_snakecase}}s
+//		@Description	Lists all {{cookiecutter.table_snakecase}}s in the database
 //		@Produce		json
-//		@Success		200	{array}		handler.{{cookiecutter.table_pascal_case}}
+//		@Success		200	{array}		handler.{{cookiecutter.table_pascalcase}}
 //		@Failure		500	{object}	map[string]string
-//		@Router			/heroes   [get]
-func List{{cookiecutter.table_pascal_case}}Handler(dbpool *pgxpool.Pool) echo.HandlerFunc {
+//		@Router			/{{cookiecutter.table_snakecase}}s   [get]
+func List{{cookiecutter.table_pascalcase}}Handler(dbpool *pgxpool.Pool) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		ctx := context.Background()
-		heroes, err := db.New(dbpool).List{{cookiecutter.table_pascal_case}}s(ctx)
-    print(heroes)
+    {{cookiecutter.table_snakecase}}s, err := db.New(dbpool).List{{cookiecutter.table_pascalcase}}s(ctx)
 		if err != nil {
-			return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to list heroes"})
+			return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to list {{cookiecutter.table_snakecase}}s"})
 		}
 
-		return c.JSON(http.StatusOK, heroes)
+		return c.JSON(http.StatusOK, {{cookiecutter.table_snakecase}}s)
 	}
 }
 
-// Create{{cookiecutter.table_pascal_case}}Handler handles the creation of a new hero.
+// Create{{cookiecutter.table_pascalcase}}Handler handles the creation of a new {{cookiecutter.table_snakecase}}.
 //
-//	@Summary		Create a new hero
-//	@Description	Creates a new hero in the database
-//	@Param			hero	body	handler.{{cookiecutter.table_pascal_case}}	true	"{{cookiecutter.table_pascal_case}} information"
+//	@Summary		Create a new {{cookiecutter.table_snakecase}}
+//	@Description	Creates a new {{cookiecutter.table_snakecase}} in the database
+//	@Param			{{cookiecutter.table_snakecase}}	body	handler.{{cookiecutter.table_pascalcase}}	true	"{{cookiecutter.table_pascalcase}} information"
 //	@Produce		json
-//	@Success		201	{object}	handler.{{cookiecutter.table_pascal_case}}
+//	@Success		201	{object}	handler.{{cookiecutter.table_pascalcase}}
 //	@Failure		400	{object}	map[string]string
 //	@Failure		500	{object}	map[string]string
-//	@Router			/heroes   [post]
-func Create{{cookiecutter.table_pascal_case}}Handler(dbpool *pgxpool.Pool) echo.HandlerFunc {
+//	@Router			/{{cookiecutter.table_snakecase}}s   [post]
+func Create{{cookiecutter.table_pascalcase}}Handler(dbpool *pgxpool.Pool) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		var hero {{cookiecutter.table_pascal_case}}
-		if err := c.Bind(&hero); err != nil {
+		var {{cookiecutter.table_snakecase}} {{cookiecutter.table_pascalcase}}
+		if err := c.Bind(&{{cookiecutter.table_snakecase}}); err != nil {
 			return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid request payload"})
 		}
 
 		ctx := context.Background()
 
     // TODO: try to DRY this part
-    if hero.ID == nil {
-      result, err := db.New(dbpool).Create{{cookiecutter.table_pascal_case}}(ctx, hero.Name)
+    if {{cookiecutter.table_snakecase}}.ID == nil {
+      result, err := db.New(dbpool).Create{{cookiecutter.table_pascalcase}}(ctx, {{cookiecutter.table_snakecase}}.Name)
       if err != nil {
-        return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to create hero"})
+        return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to create {{cookiecutter.table_snakecase}}"})
       }
       return c.JSON(http.StatusCreated, result)
     } else {
-      arg := db.Create{{cookiecutter.table_pascal_case}}WithIDParams{
-        ID:   int32(*hero.ID),
-        Name: hero.Name,
+      arg := db.Create{{cookiecutter.table_pascalcase}}WithIDParams{
+        ID:   int32(*{{cookiecutter.table_snakecase}}.ID),
+        Name: {{cookiecutter.table_snakecase}}.Name,
       }
-      result, err := db.New(dbpool).Create{{cookiecutter.table_pascal_case}}WithID(ctx, arg)
+      result, err := db.New(dbpool).Create{{cookiecutter.table_pascalcase}}WithID(ctx, arg)
       if err != nil {
-        return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to create hero"})
+        return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to create {{cookiecutter.table_snakecase}}"})
       }
       return c.JSON(http.StatusCreated, result)
     }
   }
 }
-// Get{{cookiecutter.table_pascal_case}}Handler retrieves a hero by ID.
+// Get{{cookiecutter.table_pascalcase}}Handler retrieves a {{cookiecutter.table_snakecase}} by ID.
 //
-//	@Summary		Get a hero by ID
-//	@Description	Retrieves a hero from the database by its ID
-//	@Param			id	path	int	true	"{{cookiecutter.table_pascal_case}} ID"
+//	@Summary		Get a {{cookiecutter.table_snakecase}} by ID
+//	@Description	Retrieves a {{cookiecutter.table_snakecase}} from the database by its ID
+//	@Param			id	path	int	true	"{{cookiecutter.table_pascalcase}} ID"
 //	@Produce		json
-//	@Success		200	{object}	handler.{{cookiecutter.table_pascal_case}}
+//	@Success		200	{object}	handler.{{cookiecutter.table_pascalcase}}
 //	@Failure		400	{object}	map[string]string
 //	@Failure		404	{object}	map[string]string
 //	@Failure		500	{object}	map[string]string
-//	@Router			/heroes/{id}   [get]
-func Get{{cookiecutter.table_pascal_case}}Handler(dbpool *pgxpool.Pool) echo.HandlerFunc {
+//	@Router			/{{cookiecutter.table_snakecase}}s/{id}   [get]
+func Get{{cookiecutter.table_pascalcase}}Handler(dbpool *pgxpool.Pool) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		id, err := strconv.Atoi(c.Param("id"))
 		if err != nil {
@@ -99,69 +99,69 @@ func Get{{cookiecutter.table_pascal_case}}Handler(dbpool *pgxpool.Pool) echo.Han
 		}
 
 		ctx := context.Background()
-		result, err := db.New(dbpool).Get{{cookiecutter.table_pascal_case}}(ctx, int32(id))
+		result, err := db.New(dbpool).Get{{cookiecutter.table_pascalcase}}(ctx, int32(id))
 		if err == sql.ErrNoRows {
-			return c.JSON(http.StatusNotFound, map[string]string{"error": "{{cookiecutter.table_pascal_case}} not found"})
+			return c.JSON(http.StatusNotFound, map[string]string{"error": "{{cookiecutter.table_pascalcase}} not found"})
 		} else if err != nil {
-			return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to fetch hero"})
+			return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to fetch {{cookiecutter.table_snakecase}}"})
 		}
 
 		return c.JSON(http.StatusOK, result)
 	}
 }
 
-// Update{{cookiecutter.table_pascal_case}}Handler updates a hero by ID.
+// Update{{cookiecutter.table_pascalcase}}Handler updates a {{cookiecutter.table_snakecase}} by ID.
 //
-//	@Summary		Update a hero
-//	@Description	Updates a hero in the database by its ID
-//	@Param			id		path	int				true	"{{cookiecutter.table_pascal_case}} ID"
-//	@Param			hero	body	handler.{{cookiecutter.table_pascal_case}}	true	"{{cookiecutter.table_pascal_case}} information"
+//	@Summary		Update a {{cookiecutter.table_snakecase}}
+//	@Description	Updates a {{cookiecutter.table_snakecase}} in the database by its ID
+//	@Param			id		path	int				true	"{{cookiecutter.table_pascalcase}} ID"
+//	@Param			{{cookiecutter.table_snakecase}}	body	handler.{{cookiecutter.table_pascalcase}}	true	"{{cookiecutter.table_pascalcase}} information"
 //	@Produce		json
-//	@Success		200	{object}	map[string]string	//	Success		message	can			be	more		descriptive	here	(e.g., "{{cookiecutter.table_pascal_case}} updated successfully")
+//	@Success		200	{object}	map[string]string	//	Success		message	can			be	more		descriptive	here	(e.g., "{{cookiecutter.table_pascalcase}} updated successfully")
 //	@Failure		400	{object}	map[string]string	//	Detail		error	messages	for	bad			requests
 //	@Failure		404	{object}	map[string]string	//	Specific	error	message		for	not			found
 //	@Failure		500	{object}	map[string]string	//	Generic		error	message		for	internal	errors
-//	@Router			/heroes/{id}   [put]
-func Update{{cookiecutter.table_pascal_case}}Handler(dbpool *pgxpool.Pool) echo.HandlerFunc {
+//	@Router			/{{cookiecutter.table_snakecase}}s/{id}   [put]
+func Update{{cookiecutter.table_pascalcase}}Handler(dbpool *pgxpool.Pool) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		id, err := strconv.Atoi(c.Param("id"))
 		if err != nil {
 			return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid ID in the request"})
 		}
 
-		var updated{{cookiecutter.table_pascal_case}} {{cookiecutter.table_pascal_case}}
-		if err := c.Bind(&updated{{cookiecutter.table_pascal_case}}); err != nil {
+		var updated{{cookiecutter.table_pascalcase}} {{cookiecutter.table_pascalcase}}
+		if err := c.Bind(&updated{{cookiecutter.table_pascalcase}}); err != nil {
 			return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid request payload"})
 		}
 
 		ctx := context.Background()
-		arg := db.Update{{cookiecutter.table_pascal_case}}Params{
+		arg := db.Update{{cookiecutter.table_pascalcase}}Params{
 			ID:   int32(id),
-			Name: updated{{cookiecutter.table_pascal_case}}.Name,
+			Name: updated{{cookiecutter.table_pascalcase}}.Name,
 		}
-		err = db.New(dbpool).Update{{cookiecutter.table_pascal_case}}(ctx, arg)
+		err = db.New(dbpool).Update{{cookiecutter.table_pascalcase}}(ctx, arg)
 		if err == sql.ErrNoRows {
-			return c.JSON(http.StatusNotFound, map[string]string{"error": "{{cookiecutter.table_pascal_case}} not found"})
+			return c.JSON(http.StatusNotFound, map[string]string{"error": "{{cookiecutter.table_pascalcase}} not found"})
 		} else if err != nil {
-			return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to update hero"})
+			return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to update {{cookiecutter.table_snakecase}}"})
 		}
 
-		return c.JSON(http.StatusOK, map[string]string{"message": "{{cookiecutter.table_pascal_case}} updated successfully"})
+		return c.JSON(http.StatusOK, map[string]string{"message": "{{cookiecutter.table_pascalcase}} updated successfully"})
 	}
 }
 
-// Delete{{cookiecutter.table_pascal_case}}Handler deletes a hero by ID.
+// Delete{{cookiecutter.table_pascalcase}}Handler deletes a {{cookiecutter.table_snakecase}} by ID.
 //
-//	@Summary		Delete a hero
-//	@Description	Deletes a hero from the database by its ID
-//	@Param			id	path	int	true	"{{cookiecutter.table_pascal_case}} ID"
+//	@Summary		Delete a {{cookiecutter.table_snakecase}}
+//	@Description	Deletes a {{cookiecutter.table_snakecase}} from the database by its ID
+//	@Param			id	path	int	true	"{{cookiecutter.table_pascalcase}} ID"
 //	@Produce		json
 //	@Success		204
 //	@Failure		400	{object}	map[string]string	//	Detail		error	messages	for	bad			requests
 //	@Failure		404	{object}	map[string]string	//	Specific	error	message		for	not			found
 //	@Failure		500	{object}	map[string]string	//	Generic		error	message		for	internal	errors
-//	@Router			/heroes/{id}   [delete]
-func Delete{{cookiecutter.table_pascal_case}}Handler(dbpool *pgxpool.Pool) echo.HandlerFunc {
+//	@Router			/{{cookiecutter.table_snakecase}}s/{id}   [delete]
+func Delete{{cookiecutter.table_pascalcase}}Handler(dbpool *pgxpool.Pool) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		id, err := strconv.Atoi(c.Param("id"))
 		if err != nil {
@@ -169,11 +169,11 @@ func Delete{{cookiecutter.table_pascal_case}}Handler(dbpool *pgxpool.Pool) echo.
 		}
 
 		ctx := context.Background()
-		err = db.New(dbpool).Delete{{cookiecutter.table_pascal_case}}(ctx, int32(id))
+		err = db.New(dbpool).Delete{{cookiecutter.table_pascalcase}}(ctx, int32(id))
 		if err == sql.ErrNoRows {
-			return c.JSON(http.StatusNotFound, map[string]string{"error": "{{cookiecutter.table_pascal_case}} not found"})
+			return c.JSON(http.StatusNotFound, map[string]string{"error": "{{cookiecutter.table_pascalcase}} not found"})
 		} else if err != nil {
-			return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to delete hero"})
+			return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to delete {{cookiecutter.table_snakecase}}"})
 		}
 
 		return c.NoContent(http.StatusNoContent)
