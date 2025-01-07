b2bd961766081b1c3648e072d0915abab04afe86
Pedro Flores <pdavidflores@gmail.com>
2025-01-07 06:51:22 -0300

    typo in schema.sql fixed

--- {{cookiecutter.project_slug}}/db/schema.sql
+++ {{cookiecutter.project_slug}}/db/schema.sql
@@ -2,7 +2,7 @@
  CREATE TABLE "{{table["table_snakecase"]}}" (
   {% for col in table["columns"] %}
 {# ################################################################################ #}
-{# TODO: Inferir el tipo de dato en vez de asumir que esd SERIAL (integer + seq)    #}
+{# TODO: Inferir el tipo de dato en vez de asumir que es SERIAL (integer + seq)    #}
 {# ################################################################################ #}
     {% if col["primary_key"] %}
   "{{col["column_name_snakecase"]}}" SERIAL PRIMARY KEY{% if not loop.last %},{% endif %}
